0610f5d0dce43b90a27d722b1163bc6bbae3d14d
AnalectsOfConfucius <EssaysAndCriticism@gmail.com>
2017-01-05 22:20:31 +0800

    update

---
+++ src/test/java/Test.java
@@ -0,0 +1,15 @@
+import com.hg.core.util.MD5Util;
+import com.hg.core.util.UUIdUtil;
+
+/**
+ * Created by StoryInStone on 2017/1/5.
+ */
+public class Test {
+
+    public static void main(String[] arg0) {
+        System.out.println(MD5Util.getMD5("123456"));
+        System.out.println(UUIdUtil.getUUID());
+
+    }
+
+}
